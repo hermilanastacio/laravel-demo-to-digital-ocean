3727a42c41cd0092db0baf54a1637dd89dce5c05
Hermil Gil Anastacio <hermilanastacio@pinion.ph>
2021-07-24 14:28:39 +0800

    - Minor

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,12 +16,14 @@ class PostController extends Controller
     {
         if(!empty($request->image)) {
             $image_name = str_replace('public/', '', $request->image->store('public/images'));
-            $request['image_url'] = 'https://dev.hermilanastacio.info/storage/images/'.$image_name.'';
+            $request['image_url'] = 'https://dev.hermilanastacio.info/storage/'.$image_name.'';
+
+        return $request;
+
         } else {
             $request['image_url'] = '';
         }
 
-        return Post::create($request->all());
     }
 
     public function show($id)
